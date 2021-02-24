880251295aa878362cdeaf11fd7aced047958ee6
Murilo-V <alveiramurilo@hotmail.com>
2021-02-24 13:38:09 -0300

    feat: countdown disabled and finished

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <script src='https://kit.fontawesome.com/a076d05399.js' crossOrigin='anonymous'></script>
                     <link rel="shortcut icon" href="favicon.png" type="image/png"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com"/>
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet"/>
