716eac0a32d53232ce8372752516e53e3477bd43
Murilo-V <alveiramurilo@hotmail.com>
2021-02-24 12:47:15 -0300

    feat: countdown works

---
+++ src/components/Profile.tsx
@@ -0,0 +1,14 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/murilo-v.png" alt="Profile Picture"/>
+            <div>
+                <strong>Murilo V.</strong>
+                
+                <p><img src="icons/level.svg" alt="Level Up Image"/>Level 1</p>
+            </div>
+        </div>
+    );
+}
